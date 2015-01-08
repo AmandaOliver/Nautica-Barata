0577f16d9eab726b799dfa827f6429ab62410730
antojer <anjesarol@gmail.com>
2015-01-08 13:04:07 -0800

    horario comercial final

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,56 +18,56 @@ angular
     'ngTouch'
   ])
     .config(function ($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'views/index.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/cart', {
-                    templateUrl: 'views/cart.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/checkout', {
-                    templateUrl: 'views/checkout.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/login', {
-                    templateUrl: 'views/login.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/shop', {
-                    templateUrl: 'views/shop.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/product-details', {
-                    templateUrl: 'views/product-details.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/blog-single', {
-                    templateUrl: 'views/blog-single.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/blog', {
-                    templateUrl: 'views/blog.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/404', {
-                        templateUrl: 'views/404.html',
-                        controller: 'MainCtrl'
-                    }
-                    .when('/contact-us', {
-                        templateUrl: 'views/contact-us.html',
-                        controller: 'MainCtrl'
-                    })
-                    .when('/c-env', {
-                        templateUrl: 'views/condiciones-envio.html',
-                        controller: 'MainCtrl'
-                    })
-                    .when('/horario', {
-                        templateUrl: 'views/horario.html',
-                        controller: 'MainCtrl'
-                    })
-                    .otherwise({
-                        redirectTo: '/'
-                    });
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/index.html',
+                controller: 'MainCtrl'
+            })
+            .when('/cart', {
+                templateUrl: 'views/cart.html',
+                controller: 'MainCtrl'
+            })
+            .when('/checkout', {
+                templateUrl: 'views/checkout.html',
+                controller: 'MainCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'MainCtrl'
+            })
+            .when('/shop', {
+                templateUrl: 'views/shop.html',
+                controller: 'MainCtrl'
+            })
+            .when('/product-details', {
+                templateUrl: 'views/product-details.html',
+                controller: 'MainCtrl'
+            })
+            .when('/blog-single', {
+                templateUrl: 'views/blog-single.html',
+                controller: 'MainCtrl'
+            })
+            .when('/blog', {
+                templateUrl: 'views/blog.html',
+                controller: 'MainCtrl'
+            })
+            .when('/404', {
+                templateUrl: 'views/404.html',
+                controller: 'MainCtrl'
+            })
+            .when('/contact-us', {
+                templateUrl: 'views/contact-us.html',
+                controller: 'MainCtrl'
+            })
+            .when('/c-env', {
+                templateUrl: 'views/condiciones-envio.html',
+                controller: 'MainCtrl'
+            })
+            .when('/horario-com', {
+                templateUrl: 'views/horario.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
             });
+    });
