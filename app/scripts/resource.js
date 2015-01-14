9be793c93646e0ac5ce8e2df4bad00523fb7ae48
triplew7777 <diegomanuel.mateosgomez@alum.uca.es>
2015-01-14 19:58:28 +0100

    modificado resource

--- app/scripts/resource.js
+++ app/scripts/resource.js
@@ -1,11 +1,20 @@
 angular.module('getCategoria', ['ngResource']=
     .factory('categoría', function($resource) {
-        var Productos =  $resource('https://api.mongolab.com/api/1/databases/nautica/collections/categorias/', {
+        var Categorias =  $resource('https://api.mongolab.com/api/1/databases/nautica/collections/categorias/', {
             apiKey: 'T7WNQT6J_A_tcu-F2O7TzNTrhPYVdlkA ',
             id: 'DiegoManuel'
         });
 
-    return Productos;
+    return Categorias;
+
+})
+
+    .controller('contCategorias', funcion ($scope, Categorias) {
+
+                $scope.categorias = Categorias.query({});
+
+                //Aqui se añadirian funciones para añadir y quitar categorias
+
 
 
 
